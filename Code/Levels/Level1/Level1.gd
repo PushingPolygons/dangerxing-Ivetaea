657673a2c765f1edd86ce21c01dc16f4b6d93c0e
Ivetaea <108443001+Ivetaea@users.noreply.github.com>
2022-08-15 22:48:23 -0700

    CrawlerDungeon setup
    Set up assets for one dungeon room, main player character (CRAWLER), and planned out required steps in coding needed.

---
+++ Code/Levels/Level1/Level1.gd
@@ -0,0 +1,30 @@
+extends Spatial
+# This script should include - 
+
+# 1: When spawning - activate four hit detection zones in the doorways.
+# If the detection zone finds an adjacent room, remove Door Blocker object in 
+# that cardinal direction.
+
+# 2: Spawn creatures - Spawn creatures at creature spawn points
+# (OPTIONAL) - randomize which creature spawns at each point
+
+# 3: Spawn Gems/Chest - Spawn a gem at each gem location, spawn the room's chest.
+# (OPTIONAL) - randomize gem locations, but ensure that at least a minimum
+# number of gems spawn. Only 1 chest per room.
+
+
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
